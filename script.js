1ca26b3753884d21f96cbfcaa0e4d347340fb211
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-02-19 19:18:38 -0700

    updated HTML and js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // VAR DECLARATIONS
 
-
-
-
-
 var timeOfDay = [{
     time: '9 am',
     eventInput: "",
@@ -12,49 +8,47 @@ var timeOfDay = [{
   {
     time: '10 am',
     eventInput: "",
-    id: "10"
+    id: 10
   },
   {
     time: '11 am',
     eventInput: "",
-    id: "11"
+    id: 11
   },
   {
     time: '12 am',
     eventInput: "",
-    id: "12"
+    id: 12
   },
   {
     time: '1 pm',
     eventInput: "",
-    id: "13"
+    id: 13
   },
   {
     time: '2 pm',
     eventInput: "",
-    id: "14"
+    id: 14
   },
   {
     time: '3 pm',
     eventInput: "",
-    id: "15"
+    id: 15
   },
   {
     time: '4 pm',
     eventInput: "",
-    id: "16"
+    id: 16
   },
   {
     time: '5 pm',
     eventInput: "",
-    id: "17"
+    id: 17
   },
 ]
 
 
-localStorage.getItem(eventTarget, JSON.stringify('savedObj'))
-console.log(localStorage);
-
+    
 
 // APPENDTO - - START - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -62,33 +56,42 @@ var rootEl = $('.container');
 
 for (var t = 0; t < timeOfDay.length; t++) {
 
+  rowEl = $('<div>')
+  rowEl.addClass(' row hourRow row.custom col-12')
+  rowEl.attr('id', timeOfDay[t].id);
+  rowEl.appendTo('.container')  
+  
+
   var textBlock = $('<div>');
   var tbText = $('<p></p>');
   var description = $('<textarea>');
   var saveBtn = $('<button>');
 
   textBlock.text(timeOfDay[t].time)
-  // textBlock.attr('id', timeOfDay[t].id);
   saveBtn.attr('id', timeOfDay[t].id);
-  description.attr('id', timeOfDay[t].id, )
+  description.attr('id', timeOfDay[t].id)
 
-  textBlock.addClass('time-block hour col-md-2')
-  description.addClass('description col-md-8')
-  saveBtn.addClass('saveBtn i col-md-2')
-
-  textBlock.appendTo('.container');
-  description.appendTo($('.container'));
-  saveBtn.appendTo('.container');
+  textBlock.addClass('time-block hour col-2')
+  description.addClass('description col-8 ')
+  saveBtn.addClass('saveBtn i col-2')
+  
+  textBlock.appendTo(rowEl);
+  description.appendTo($(rowEl));
+  saveBtn.appendTo(rowEl);
 };
+
+
 
 
 // APPENDTO - - END - - - - - - - - - - - - - - - - - - - - - - - 
 
 var currentTime = $('#currentDay');
-var clickedSaveBtn;
+// var clickedSaveBtn;
 
 currentTime.append(moment().format('MMMM Do YYYY, h:mm:ss a'));
-console.log(currentTime);
+// console.log(currentTime);
+
+$('.saveBtn').append('SAVE \n💾')
 
 // CHANGING COLOR  - - START - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 // 
@@ -96,12 +99,12 @@ console.log(currentTime);
 function colorEl() {
 
   var hour = moment().hours();
-  console.log(hour);
+  // console.log(hour);
 
   $(".description").each(function () {
 
     var currentHour = parseInt($(this).attr('id'));
-    console.log(currentHour);
+    // console.log(currentHour);
 
     if (currentHour < hour) {
       $(this).addClass('past');
@@ -113,68 +116,43 @@ function colorEl() {
 
   })
 }
+
 // CHANGING COLOR  - - END - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
 // 
 // 
 // 
 // SAVE BUTTON - - START - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-// 
-// 
 
+localStorage.getItem('eventTarget');
 
-var userInput = [];
-var eventTarget;
-var savedObj = {};
+$('textarea').each(function () {
+  var text = ($(this).val());
+  console.log(text);
+  if (text) {
+    text = localStorage('eventTarget')
+  }
+})
 
-function saveEl() {
-  $('.saveBtn').click(function (event) {
+  rootEl.on('click', '.saveBtn', '.description', function (event) {
+
+    event.preventDefault();
+
     eventTarget = ($(this).attr('id'));
-    console.log(eventTarget);
-    $("textarea").each(function () {
-      userInput = $(event.target).parent().children('.description').val()
-      console.log(userInput)
-      savedObj = {
-        'key': eventTarget,
-        'input': userInput
-      }
-    })
-    localStorage.setItem(eventTarget, JSON.stringify(savedObj))
-    console.log(savedObj);
-    localStorage.getItem(eventTarget, JSON.stringify('savedObj'))
-  })
-};
+    console.log('time: ' + eventTarget);
 
+    userInput = $(event.target).parent().children('.description').val()
+    console.log('user input: ' + userInput)
 
-// 
-// 
-// SAVE TO LOCAL STORAGE - - END - - - - - - - - - - - - - - - - - - - - - - - 
+    // userObj = {
+    //   'key': eventTarget,
+    //   'value':  userInput
+    // }
 
-// function saveEl(e) {
-//   $('.saveBtn').click(function (event) {
-//     eventTarget = ($(this).attr('id'));
-//     console.log(eventTarget);
-//     $("textarea").each(function (e) {
-//       userInput = $(event.target).parent().children('.description').val();
-//       console.log(userInput)
-//       savedObj = {
-//         'key': eventTarget,
-//         'input': userInput
-//       }
-//       localStorage.setItem(eventTarget, JSON.stringify(savedObj))
-//       console.log(savedObj);
-//     })
-//     alert("All Data Saved")
-//   })
-// };
+    localStorage.setItem(eventTarget, userInput)
+    console.log(localStorage);
+})
 
-// localStorage.getItem( function().on(saveEl() {
-
-//   localStorage.setItem(eventTarget, JSON.stringify(savedObj))
-// })
-  
-
-
-
-
-colorEl();
-saveEl();
+colorEl()
